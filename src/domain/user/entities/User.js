b685b3618877bad6064c6955f09d809e4b5ae788
Manuel Florez <manuelflorezw@gmail.com>
2024-04-01 00:06:37 -0500

    use ExceptionUser in class User

--- src/domain/user/entities/User.js
+++ src/domain/user/entities/User.js
@@ -1,19 +1,21 @@
+import ExceptionUser from "src/domain/user/exceptions/ExceptionUser.js"
+
 export default class User {
   constructor(id, username, password) {
     if(!username) {
-      throw Error(`User need a username: ${username}`)
+      throw new ExceptionUser(`User need a username: ${username}`)
     }
     if(!password) {
-      throw Error(`User need a password: ${password}`)
+      throw new ExceptionUser(`User need a password: ${password}`)
     }
     if(typeof id !== 'number') {
-      throw Error(`User need a id: ${id}`)
+      throw new ExceptionUser(`User need a id: ${id}`)
     }
     if(username.length < 5) {
-      throw Error(`User need a username with more than 4 characters: ${username}`)
+      throw new ExceptionUser(`User need a username with more than 4 characters: ${username}`)
     }
     if(password.length < 7) {
-      throw Error(`User need a password with more than 6 characters: ${password}`)
+      throw new ExceptionUser(`User need a password with more than 6 characters: ${password}`)
     }
     this.username = username
     this.password = password
