bb08c064ebb9e1f593a7072d7593887409662ef4
Manuel <ManuelFlorez@users.noreply.github.com>
2024-03-31 23:35:35 -0500

    Update set-user.sh

--- script/set-user.sh
+++ script/set-user.sh
@@ -5,15 +5,4 @@ password=$2
 
 data="{\"id\":0,\"username\":\"${username}\",\"password\":\"${password}\"}"
 
-echo "${data}"
-
-#data='{"user":"'
-#data+=$user
-#data+='","password":"'
-#data+=$password
-#data+='"}'
-
-curl -s -X POST -H 'Content-Type: application/json' \
-  -d $data \
-  localhost:3000/api/v1/user \
-	| python3 -m json.tool
+curl -s -X POST -H 'Content-Type: application/json' -d $data localhost:3000/api/v1/user | python3 -m json.tool
