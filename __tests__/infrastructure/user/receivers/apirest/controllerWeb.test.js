b29e30becd3cf659bdefa793e2bd3dd44e5169e0
Manuel Florez <manuelflorezw@gmail.com>
2024-04-03 11:53:02 -0500

    add test create user

--- __tests__/infrastructure/user/receivers/apirest/controllerWeb.test.js
+++ __tests__/infrastructure/user/receivers/apirest/controllerWeb.test.js
@@ -10,6 +10,7 @@ describe('class ControllerWeb', () => {
     test('should returns Array empty', async () => {
       const controllerWeb = new ControllerWeb({ findAll: () => Promise.resolve([]) })
       const app = express()
+      app.use(express.json())
       app.use('/user', controllerWeb.router)
       const response = await request(app).get('/user')
       expect(response.headers['content-type']).toBe('application/json; charset=utf-8')
@@ -22,6 +23,7 @@ describe('class ControllerWeb', () => {
     test('should returns ExceptionUser when create a User', async () => {
       const controllerWeb = new ControllerWeb()
       const app = express()
+      app.use(express.json())
       app.use('/user', controllerWeb.router)
       const response = await request(app).post('/user').send({})
       expect(response.headers['content-type']).toBe('application/json; charset=utf-8')
@@ -29,6 +31,24 @@ describe('class ControllerWeb', () => {
       expect(response.body.message).toBe(`User need a username: undefined`)
       expect(response.body.status).toEqual(404)
       expect(response.status).toBe(404)
+    })
+
+    test('should return user when create successful a User', async () => {
+      const controllerWeb = new ControllerWeb({ create: (user) => {
+        user.id = 1
+        return Promise.resolve(user) }
+      })
+      const app = express()
+      app.use(express.json())
+      app.use('/user', controllerWeb.router)
+      const payload = { username: 'Manuel', password: '12345678' }
+      const response = await request(app).post('/user').send(payload)
+      expect(response.headers['content-type']).toBe('application/json; charset=utf-8')
+      expect(response.headers['content-length']).toBe('90')
+      const result = { id: 1, username: 'Manuel', password: '12345678' }
+      expect(response.body.data.user).toEqual(result)
+      expect(response.body.status).toEqual('successful')
+      expect(response.status).toBe(200)
     })
   
   })
